befca0433c419fc7e93e90b32acb4fe165a0600a
GwenCodes <essexdesigns43.0@gmail.com>
2025-10-20 13:39:09 -0400

    Started Weather.js function

---
+++ src/Weather.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+  const [city, setCity] = useState(props.defaultCity);
+
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
