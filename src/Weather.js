2e9a52756b8515b08bd4bc9e73824f84a8b37dec
GwenCodes <essexdesigns43.0@gmail.com>
2025-10-21 21:19:57 -0400

    Installed axios and useState for Weather.js function

--- src/Weather.js
+++ src/Weather.js
@@ -1,48 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather(props) {
-  return (
-    <div className="Weather">
-      <form>
+export default function Weather() {
+  const [ready, setReady] = useState(false);
+  const [temperature, setTemperature] = useState(null);
+  function handleResponse(response) {
+    console.log(response.data);
+    setTemperature(response.data.temperature.current);
+    setReady(true);
+  }
+
+  if (ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city..."
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="Search" className="btn btn-primary" />
+            </div>
+          </div>
+        </form>
+        <h1>New York</h1>
+        <ul>
+          <li>Monday 7:00</li>
+          <li>Sunny</li>
+        </ul>
         <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city..."
-              className="form-control"
-              autoFocus="on"
-            />
+          <div className="col-6">
+            <div className="clearfix" mt-4>
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+                alt="sunny"
+                className="float-start"
+              />
+              <span className="temperature">25</span>
+              <span className="unit">°C</span>
+            </div>
           </div>
-          <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+          <div className="col-6">
+            <div className="float-start">
+              <ul>
+                <li>Humidity: 60%</li>
+                <li>Wind: 10 km/h</li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </form>
-      <h1>New York</h1>
-      <ul>
-        <li>Monday 7:00</li>
-        <li>Sunny</li>
-      </ul>
-      <div className="row" mt-4 align-items-center>
-        <div className="col-6">
-          <div className="clearfix">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
-              alt="sunny"
-              className="float-left"
-            />
-            <span className="temperature">25</span>
-            <span className="unit">°C</span>
-          </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Humidity: 60%</li>
-            <li>Wind: 10 km/h</li>
-          </ul>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "61dfa07e8o8462teba3fadad30e70d77";
+    let city = "New York";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading...";
+  }
 }
