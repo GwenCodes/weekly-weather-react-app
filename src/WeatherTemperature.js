13e3056ac0dad24ba4388ca14bbcdfded0da684c
GwenCodes <essexdesigns43.0@gmail.com>
2025-10-30 17:27:04 -0400

    Removed celcius temperature and useState

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,50 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Weather.css";
 
 export default function WeatherTemperature(props) {
-  // keep unit spelled consistently as "celsius"
-  const [unit, setUnit] = useState("celsius");
+  const fahrenheit = (props.celsius * 9) / 5 + 32;
 
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  if (unit === "celsius") {
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(props.celsius)}</span>
-        <span className="unit">
-          °C |{" "}
-          <a
-            href="/fahrenheit"
-            style={{ textDecoration: "none" }}
-            onClick={showFahrenheit}>
-            °F
-          </a>
-        </span>
-      </div>
-    );
-  } else {
-    let fahrenheit = (props.celsius * 9) / 5 + 32;
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(fahrenheit)}</span>
-        <span className="unit">
-          <a
-            href="/celcius"
-            style={{ textDecoration: "none" }}
-            onClick={showCelsius}>
-            °C
-          </a>{" "}
-          | °F
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="WeatherTemperature">
+      <span className="temperature">{Math.round(fahrenheit)}</span>
+      <span className="unit">°F</span>
+    </div>
+  );
 }
